f77bbe5c00268de9edee74ca1885b67884a4547d
MohammadHosseinKv <Mohammadhossein.kv@gmail.com>
2025-10-25 00:46:53 +0330

    update config to build multiple pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import { multi } from 'vite-plugin-multi-pages';
 
 export default defineConfig({
-  base: './'
-})
+  base: './',
+  plugins: [
+    multi({
+      pages: {
+        index: 'index.html',
+        404: '404.html',
+      }
+    })
+  ]
+});
